662681512ef491fe013a63102216fd6ae694b6b1
mshuva <mshuva@umich.edu>
2015-11-14 22:48:39 -0500

    Functions and scripts

--- convert2double.m
+++ convert2double.m
@@ -1,8 +1,9 @@
 %% This function converts all the uint8 images to doubles in order to form 
-%  the data matrix.
+%  the data matrix. Resize image to 0.25 the original size.
 
 function imDouble = convert2double(image_array)
 	for i = 1:length(image_array)
-	    imDouble{i} = im2double(rgb2gray(image_array{i}));
+	    image2D = im2double(rgb2gray(image_array{i}));
+        imDouble{i}= imresize(image2D,0.25);
 	end
 end
